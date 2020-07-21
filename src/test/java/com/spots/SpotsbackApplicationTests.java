d3cd526ba09e07fa00c9b7fea8d10a08b06faa40
Hyunmin <hunmin8788@naver.com>
2020-07-21 17:28:54 +0900

    commit

---
+++ src/test/java/com/spots/SpotsbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotsbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
