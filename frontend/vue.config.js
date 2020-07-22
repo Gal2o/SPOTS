23ce2b2388b4f656afbebc5371e882a7c3d6efef
jjk8060 <jjk8060@naver.com>
2020-07-22 13:26:37 +0900

    LoginButtonDisableTest

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -20,5 +20,10 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
+  },
+  module.exports = {
+    devServer: {
+        overlay: false
+    }
   }
 };
