add7b6452da71a4e724457ae2487064e49ff4d31
ojywjdduq <ojywjdduq@naver.com>
2020-07-22 17:06:51 +0900

    backend_login_demofile

---
+++ backend/src/main/java/com/spots/service/UserInfoService.java
@@ -0,0 +1,19 @@
+package com.spots.service;
+
+import java.util.List;
+
+import com.ssafy.happyhouse.dto.FavInfo;
+import com.ssafy.happyhouse.dto.UserInfo;
+
+public interface UserInfoService {
+	
+	public int signUp(UserInfo dto);
+	public int modify(UserInfo dto);
+	
+	public int delete(String id);
+	
+	public UserInfo findPwdById(String id);
+	public UserInfo detail(String id);
+	public int registFav(FavInfo favInfo);
+	public List<FavInfo> userFavList(String id);
+}
