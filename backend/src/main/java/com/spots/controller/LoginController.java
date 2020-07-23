e258eb4d52351ffd139f4243a584117127e9975e
ojywjdduq <ojywjdduq@naver.com>
2020-07-23 17:47:55 +0900

    crossorigin

--- backend/src/main/java/com/spots/controller/LoginController.java
+++ backend/src/main/java/com/spots/controller/LoginController.java
@@ -1,31 +1,36 @@
 package com.spots.controller;
 
-import java.util.HashMap;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.spots.dto.UserInfo;
 import com.spots.service.LoginService;
 
-
-
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 public class LoginController {
 	
 	@Autowired
 	LoginService loginService;
+	private static RestTemplate restTemplate;
 	
 	@ExceptionHandler
 	public ModelAndView handler(Exception ex) {
@@ -35,36 +40,23 @@ public class LoginController {
 		return mav;
 	}
 	
-	//get방식으로 /login을 받으면 application.properties에서 지정한 경로인 /WEB-INF/views/의 loginForm.jsp로 보내주는 코드
-//	@GetMapping(value="/login")
-//	public String loginForm() {
-//		return "loginForm";
-//	}
+
 	
 	@GetMapping(value="/") 
 	public String loginForm() {
 		return "user/login";
 	}
 	
-	@GetMapping(value="/login") 
-	public String test() {
-		System.out.println("들어옴3sp");
-		return "들어옴";
-	}
 	
 	//post방식으로 /login을 받았을때 같이 보내진 id와 password를 가지고 회원 확인함
 	@PostMapping(value="/login")
-	public Map<String, Object> login(@RequestParam Map<String, String> param) {
+	public UserInfo login(@RequestParam String email, @RequestParam String password) {
+	    
 		System.out.println("들어옴");
-		System.out.println(param.get("email"));
-		String email =  param.get("email");
-		String password = param.get("password");
-		System.out.println(loginService.login(email, password));
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("message", "Server message with ");
-        return map;
-        //return loginService.login(email, password);
 
+		System.out.println(loginService.login(email, password));
+
+        return loginService.login(email, password);
 	}
 	
 	@RequestMapping(value="/logout")
