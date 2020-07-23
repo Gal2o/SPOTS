059db0c507039028d33b68ab226c168d63ba0b4f
이동옥 <okdong23@naver.com>
2020-07-23 10:57:00 +0900

    profile

---
+++ backend/src/main/java/com/spots/BackendGradleApplication.java
@@ -0,0 +1,13 @@
+package com.spots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGradleApplication.class, args);
+	}
+
+}
