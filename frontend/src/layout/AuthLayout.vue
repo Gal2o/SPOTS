df2d699ac8f8a4e88650a29da131cbc072007122
jjk8060 <jjk8060@naver.com>
2020-07-23 10:26:05 +0900

    footerFinal

--- frontend/src/layout/AuthLayout.vue
+++ frontend/src/layout/AuthLayout.vue
@@ -88,25 +88,23 @@
                 <div class="row align-items-center justify-content-xl-between">
                     <div class="col-xl-6">
                         <div class="copyright text-center text-xl-left text-muted">
-                            &copy; {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1"
-                                               target="_blank">Creative Tim</a>
+                            &copy; {{year}} <a href="https://lab.ssafy.com/s03-webmobile2-sub2/s03p12a408" class="font-weight-bold ml-1"
+                                               target="_blank">제작 : 일검사회</a>
                         </div>
                     </div>
                     <div class="col-xl-6">
                         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
                             <li class="nav-item">
-                                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                                <router-link to="/">
+                                    <a href="#" class="nav-link" target="_blank">SPOTs</a>
+                                </router-link>
                             </li>
                             <li class="nav-item">
-                                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About
-                                    Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+                                <a href="https://lab.ssafy.com/s03-webmobile2-sub2/s03p12a408" class="nav-link" target="_blank">일검사회</a>
                             </li>
                             <li class="nav-item">
                                 <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md"
-                                   class="nav-link" target="_blank">MIT License</a>
+                                   class="nav-link" target="_blank">MIT 라이센스</a>
                             </li>
                         </ul>
                     </div>
--- frontend/src/layout/ContentFooter.vue
+++ frontend/src/layout/ContentFooter.vue
@@ -3,16 +3,18 @@
     <div class="row align-items-center justify-content-lg-between">
       <div class="col-lg-6">
         <div class="copyright text-center text-lg-left text-muted">
-          © {{year}} <a href="#" class="font-weight-bold ml-1" target="_blank">제작 : 일검사회</a>
+          © {{year}} <a href="https://lab.ssafy.com/s03-webmobile2-sub2/s03p12a408" class="font-weight-bold ml-1" target="_blank">제작 : 일검사회</a>
         </div>
       </div>
       <div class="col-lg-6">
         <ul class="nav nav-footer justify-content-center justify-content-lg-end">
           <li class="nav-item">
-            <a href="#" class="nav-link" target="_blank">SPOTs</a>
+            <router-link to="/">
+              <a href="#" class="nav-link" target="_blank">SPOTs</a>
+            </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link" target="_blank">일검사회</a>
+            <a href="https://lab.ssafy.com/s03-webmobile2-sub2/s03p12a408" class="nav-link" target="_blank">일검사회</a>
           </li>
           <li class="nav-item">
             <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">템플릿 라이센스</a>
