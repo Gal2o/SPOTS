89f0f314c6c16a40109ec6da2ec65152ee6304a8
이동옥 <okdong23@naver.com>
2020-08-03 22:02:16 +0900

    ranksystem

---
+++ frontend/src/views/Tables/teamRanking.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="card shadow" :class="type === 'dark' ? 'bg-default' : ''">
+    <div
+      class="card-header border-0"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    >
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0" :class="type === 'dark' ? 'text-white' : ''">
+            {{ this.title }}
+          </h3>
+        </div>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <base-table
+        class="table align-items-center table-flush"
+        :class="type === 'dark' ? 'table-dark' : ''"
+        :thead-classes="type === 'dark' ? 'thead-dark' : 'thead-light'"
+        tbody-classes="list"
+        :data="FreerankData"
+      >
+        <template slot="columns">
+          <th>팀 이름</th>
+          <th>승리</th>
+          <th>무승부</th>
+          <th>패배</th>
+          <th>승률</th>
+          <th></th>
+        </template>
+
+        <template slot-scope="{ row }">
+          <th scope="row">
+            <div class="media align-items-center">
+              <div class="media-body">
+                <span class="name mb-0 text-sm"> {{ row.team_name }}</span>
+              </div>
+            </div>
+          </th>
+          <td>
+            <span class="status">{{ row.team_win }}</span>
+          </td>
+          <td>
+            <span class="status">{{ row.team_draw }}</span>
+          </td>
+          <td>{{ row.team_lose }}</td>
+          <td>
+            <span class="status">{{ row.team_rate }}%</span>
+          </td>
+        </template>
+      </base-table>
+    </div>
+    <div
+      class="card-footer d-flex justify-content-between"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    ></div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+const SERVER_URL = "http://localhost:8080/";
+
+export default {
+  name: "frank-table",
+  components: {},
+  created() {
+    axios
+      .get(SERVER_URL + "rank/")
+      .then((res) => {
+        this.FreerankData = res.data;
+        console.log(this.FreerankData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  props: {
+    type: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      FreerankData: [],
+      title: "팀 랭크",
+    };
+  },
+  methods: {},
+};
+</script>
+<style></style>
