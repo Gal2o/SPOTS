ff8bd2158101fb560dc0b34adb7bd67b537565fe
ojywjdduq <ojywjdduq@naver.com>
2020-08-05 18:17:12 +0900

    결재

---
+++ backend/src/main/java/com/spots/controller/KakaoPayController.java
@@ -0,0 +1,91 @@
+package com.spots.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spots.dto.ApplyTeamInfo;
+import com.spots.dto.KakaoPayReadyVO;
+import com.spots.dto.UserInfo;
+import com.spots.service.UserInfoService;
+
+@CrossOrigin(origins = {"http://192.168.0.30:3000", "http://localhost:3000"})
+@RestController
+public class KakaoPayController {
+	
+	@ExceptionHandler
+	public ModelAndView handler(Exception ex) {
+		ModelAndView  mav = new ModelAndView("error/errorHandler");
+		mav.addObject("msg", ex.getMessage());
+		ex.printStackTrace();
+		return mav;
+	}
+	
+	@PostMapping(value="/kakaoPay")
+	public String login(@RequestParam String price) {
+		System.out.println("통신"); 
+		KakaoPayReadyVO kakaoPayReadyVO = new KakaoPayReadyVO();
+		RestTemplate restTemplate = new RestTemplate();
+	    String HOST = "https://kapi.kakao.com";
+
+	    // 서버로 요청할 Header
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.add("Authorization", "KakaoAK " + "9270210e4065b410d1a6982e70e3713e");
+	    headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
+	    headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE+";charset=UTF-8");
+	    
+	    
+	    // 서버로 요청할 Body
+	    MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+	    params.add("cid", "TC0ONETIME");
+	    params.add("partner_order_id", "1001");
+	    params.add("partner_user_id", "zxcsf");
+	    params.add("item_name", "축구장 대여료");
+	    params.add("quantity", "1");
+	    params.add("total_amount", price);
+	    params.add("tax_free_amount", "0");
+	    params.add("approval_url", "http://localhost:8080/");
+	    params.add("cancel_url", "http://localhost:8080/");
+	    params.add("fail_url", "http://localhost:8080/");
+	    
+
+	    HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+
+	    try {
+	    	kakaoPayReadyVO = restTemplate.postForObject(new URI(HOST + "/v1/payment/ready"), body, KakaoPayReadyVO.class);
+	        System.out.println(kakaoPayReadyVO.getNext_redirect_pc_url());
+	        return kakaoPayReadyVO.getNext_redirect_pc_url();
+
+	    } catch (RestClientException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    } catch (URISyntaxException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	    
+	    return kakaoPayReadyVO.getNext_redirect_pc_url();
+	}
+	
+}
+
